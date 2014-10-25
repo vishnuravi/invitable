141046eaeaa9841e12ac6c71954b8524e9f4db47
Vishnu Ravi <vishnu@vishnu.io>
2014-10-25 19:42:45 -0400

    fix invitables route

--- routes/invitableRoute.js
+++ routes/invitableRoute.js
@@ -1,9 +1,8 @@
 var Invitables = require('./models/invitable');
 
 exports.create = function(req,res){
-  var event = new Invitables();
-  event.name=req.body.name;
-
+  var event = new Invitables({ name: req.body.name});
+ 
   event.save(function(err){
     if(err){
       res.send(err);
