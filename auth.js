87767f594bc50531fac6c3e532ab52e856c25e98
Vishnu Ravi <vishnu@vishnu.io>
2014-10-25 23:43:23 -0400

    fb/twitter auth

---
+++ auth.js
@@ -0,0 +1,21 @@
+module.exports = {
+
+	'facebookAuth' : {
+		'clientID' 		: '',
+		'clientSecret' 	: '',
+		'callbackURL' 	: 'http://invitable.vishnu.io/auth/facebook/callback'
+	},
+
+	'twitterAuth' : {
+		'consumerKey' 		: '',
+		'consumerSecret' 	: '',
+		'callbackURL' 		: 'http://invitable.vishnu.io/auth/twitter/callback'
+	},
+
+	'googleAuth' : {
+		'clientID' 		: '',
+		'clientSecret' 	: '',
+		'callbackURL' 	: 'http://invitable.vishnu.io/auth/google/callback'
+	}
+
+};
