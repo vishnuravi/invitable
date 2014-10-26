ba41bdfb78e7ead42b6cca3516487ba5aee485eb
Vishnu Ravi <vishnu@vishnu.io>
2014-10-26 01:25:01 -0400

    auth config

---
+++ config/auth.js
@@ -0,0 +1,14 @@
+module.exports = {
+
+	'facebookAuth' : {
+		'clientID' 		: '',
+		'clientSecret' 	: '',
+		'callbackURL' 	: 'http://invitable.vishnu.io/auth/facebook/callback'
+	},
+
+	'twitterAuth' : {
+		'consumerKey' 		: '',
+		'consumerSecret' 	: '',
+		'callbackURL' 		: 'http://invitable.vishnu.io/auth/twitter/callback'
+	}
+};
