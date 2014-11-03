0c50e6255a161651f249338167763f5961cabc44
Vishnu Ravi <vishnu@vishnu.io>
2014-11-03 06:17:16 -0500

    Issue #18 - More validation improvements

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
-    $('#signupForm').bootstrapValidator({
+    $('#signupForm')
+      .on('init.field.bv', function(e, data) {
+            var $parent    = data.element.parents('.form-group'),
+                $icon      = $parent.find('.form-control-feedback[data-bv-icon-for="' + data.field + '"]'),
+                options    = data.bv.getOptions(),
+                validators = data.bv.getOptions(data.field).validators;
+
+            if (validators.notEmpty && options.feedbackIcons && options.feedbackIcons.required) {
+                $icon.addClass(options.feedbackIcons.required).show();
+            }
+        })
+    .bootstrapValidator({
         container: 'popover',
     	feedbackIcons: {
             required: 'glyphicon glyphicon-asterisk',
