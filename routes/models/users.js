2722b45eed5a2443bf32a75ca13dcde752a4f09b
Vishnu Ravi <vishnu@vishnu.io>
2014-11-04 07:51:09 -0500

    Issue #21 - Update user schema for account verification

--- routes/models/users.js
+++ routes/models/users.js
@@ -8,6 +8,9 @@ var userSchema = mongoose.Schema({
     local            : {
         email        : String,
         password     : String,
+        verifyToken : String,
+        verifyExpiry : Date,
+        isVerified  : { type: Boolean, default: false },
         resetPasswordToken  : String,
         resetPasswordExpiry : Date
     },
